057e2fe0d1ae0dc1bd7cb4653d334c4a9de424dc
Jeremy Roelfs <jeremyroelfs@icloud.com>
2015-12-08 12:22:59 -0700

    Jeremy - Finished

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 //This is where you should incorporate your modules and execute your app
+
+
+//MODULES----------------------------------------------
+var fs = require('fs');
+var responseObj = require('./autoResponse.js');
+var weekday = require('weekday');
+
+
+var day = weekday();
+
+
+//Print to file
+fs.writeFile("./test", responseObj.response(day), function(err) {
+    if(err) {
+        return console.log(err);
+    }
+}); 
--- autoResponse.js
+++ autoResponse.js
@@ -1,1 +1,36 @@
 //Write your Constructor Function that will generate a different response for each day of the week
+
+
+module.exports = {
+response : function(day) {
+	switch(day)
+	{
+		case 1:
+			return 'It is Monday';
+		break;
+		case 2:
+			return 'It is Tuesday.';
+		break;
+	case 3:
+			return 'It is Wednsday';
+		break;
+	case 4:
+			return 'It is Thursday.';
+		break;
+	case 5:
+			return 'It is Friday. Beer me!';
+		break;
+	case 6:
+		return 'It is Saturday.';
+		break;
+	case 7:
+			return 'It is Sunday, watch a movie and code.';
+		break;
+	default:
+		return "You didn't select a day";
+	}//End select
+
+}//end response function
+
+
+}
